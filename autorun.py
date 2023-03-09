521f423f9083f33c3a86e1564719b5805bdd1e77
vishal-allada <vishal.allada@tigeranalytics.com>
2023-03-09 17:35:15 +0530

    initial version of script

--- autorun.py
+++ autorun.py
@@ -1,1 +1,16 @@
 import os
+
+# Add the path here
+PATH = "Data_Explorer"
+
+python_scripts = []  # list of python scripts
+
+with os.scandir(PATH) as entries:
+    for entry in entries:
+        file_name = entry.name
+        if file_name[len(file_name) - 3 :] == ".py":
+            python_scripts.append(file_name)
+
+for py_file in python_scripts:
+    os.system(PATH + py_file)
+    input("Press Enter to run the next file (if exists..)")
